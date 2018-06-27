1adfddde7ede426b2a566f97f35fc7fb6bb2aeb6
overid40 <overid40@gmail.com>
2018-06-27 15:53:22 +0900

    Initial commit

---
+++ emaillist/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from emaillist.models import Emaillist
+# Create your views here.
+
+
+def index(request):
+    emaillist_list = Emaillist.objects.all().order_by('-id')
+    data = {'emaillist_list':emaillist_list}
+    return render(request, 'index.html', data)
+
+
+def form(request):
+    return render(request, 'form.html')
+
+
+def add(request):
+    emaillist = Emaillist()
+    emaillist.first_name = request.POST['fn']
+    emaillist.last_name = request.POST['ln']
+    emaillist.email = request.POST['email']
+
+    emaillist.save()
+
+    # return render(request, 'success.html') 이렇게 하면 안 됨
+    return HttpResponseRedirect('/emaillist')
+
+
+
+
